a1cb42eaca22ba42d99986cec7771652990f1e86
jonaner <65676255+jonaner@users.noreply.github.com>
2020-06-03 13:45:24 -0400

    Update script.js

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,6 +26,7 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
 function addGreeting(){
     fetch('/data').then(response => response.text()).then((quote) => {
     document.getElementById('quote-container').innerText = quote;
