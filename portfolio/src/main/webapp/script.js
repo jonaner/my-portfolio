4bffe8e0b0a02c40a4d431ba999e3085e8958c91
Jonathan <jonathananer@google.com>
2020-06-11 15:55:45 +0000

    Fomatting script.js

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,7 +15,7 @@
 /**
  * Adds a random greeting to the page.
  */
-function addMessage() { //eslint-disable-line
+function addMessage() {  // eslint-disable-line
   fetch('/data').then((response) => response.json()).then((messages) => {
     const messageList = document.getElementById('past-messages');
     messageList.innerHTML = '';
